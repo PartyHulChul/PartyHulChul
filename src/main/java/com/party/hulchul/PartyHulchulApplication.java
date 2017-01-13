1fac3fef49836b115d4c170e1f3aefd3308d63a7
PartyHulChul <PartyHulChul@users.noreply.github.com>
2017-01-12 22:11:10 -0700

    initial version

---
+++ src/main/java/com/party/hulchul/PartyHulchulApplication.java
@@ -0,0 +1,12 @@
+package com.party.hulchul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyHulchulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyHulchulApplication.class, args);
+	}
+}
